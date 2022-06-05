7b14d17e374a254443ac774b2d1b7ba9642b3b42
icrazymob <44069831+icrazymob@users.noreply.github.com>
2022-06-05 18:54:39 +0300

    fix Customer bug

--- amocrm/v2/__init__.py
+++ amocrm/v2/__init__.py
@@ -4,6 +4,7 @@ from .entity.call import Direction as CallDuration
 from .entity.call import Status as CallStatus
 from .entity.company import Company
 from .entity.contact import Contact
+from .entity.customer import Customer
 from .entity.lead import Lead
 from .entity.pipeline import Pipeline, Status
 from .entity.tag import Tag
